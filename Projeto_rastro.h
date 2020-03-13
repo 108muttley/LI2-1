f80907efd0ae1ef42b9c747d4fb57f929e4a034d
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-03-13 16:54:07 +0000

    Estado proposto pelo guião 5

--- Projeto_rastro.h
+++ Projeto_rastro.h
@@ -5,9 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-typedef struct estado {
-    int tabuleiro [8][8]; //0- LIVRE; 1- POSIÇAO ATUAL; 2- BLOQUEADA;
-}ESTADO;
+typedef enum {VAZIO, BRANCA, PRETA} CASA;
+typedef struct {
+    int coluna;
+    int linha;
+} COORDENADA;
+
+typedef struct {
+    COORDENADA jogador1;
+    COORDENADA jogador2;
+} JOGADA;
+
+typedef JOGADA JOGADAS[32];
+
+typedef struct {
+    CASA tab[8][8];
+    COORDENADA ultima_jogada;
+    JOGADAS jogadas;
+    int num_jogadas;
+    int jogador_atual;
+} ESTADO;
 
 void gameStart (ESTADO *e);
 #endif //PROJETO_RASTRO_H
